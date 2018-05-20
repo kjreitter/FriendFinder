5cf64420df25ce55fc200426f5de52487276582f
Pinelli <npinelli@sop2776.attlocal.net>
2018-05-20 18:32:51 -0400

    push

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+
+var friendsList = [
+    {
+        name: "Derek Jeter",
+        photo: "http://cdn1.thecomeback.com/wp-content/uploads/sites/94/2017/05/175353984-e1494518791523-832x447.jpg",
+        scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+        name: "Snooki",
+        photo: "https://marriedbiography.com/wp-content/uploads/2017/02/Snooki.jpg",
+        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+        name: "Elizabeth Hurley",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Elizabeth_Hurley_2015_TCA_Press_Tour_%28cropped%29.jpg",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Dan Marino",
+        photo: "https://pbs.twimg.com/profile_images/610508560116633600/3ZGUY45Q_400x400.jpg",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        name: "Homer Simpson",
+        photo: "https://images-na.ssl-images-amazon.com/images/I/41b2Y9DaQ3L._SX342_.jpg",
+        scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+        name: "Jennifer Lawrence",
+        photo: "https://ia.media-imdb.com/images/M/MV5BOTU3NDE5MDQ4MV5BMl5BanBnXkFtZTgwMzE5ODQ3MDI@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+        name: "Barbara Walters",
+        photo: "https://pbs.twimg.com/profile_images/148206104/BW_photo_400x400.JPG",
+        scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+];
+
+module.exports = friendsData;
